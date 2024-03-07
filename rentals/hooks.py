ab7f3c57036ade4ffd7869926088bd49661e70e1
Hussain Nagaria <hussainbhaitech@gmail.com>
2024-03-07 14:29:24 +0400

    fix: 😭

--- rentals/hooks.py
+++ rentals/hooks.py
@@ -28,7 +28,7 @@ app_license = "mit"
 # page_js = {"page" : "public/js/file.js"}
 
 # include js in doctype views
-doctype_js = {"User" : "public/js/doctype.js"}
+# doctype_js = {"User" : "public/js/doctype.js"}
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
@@ -67,8 +67,8 @@ doctype_js = {"User" : "public/js/doctype.js"}
 # Installation
 # ------------
 
-before_install = "rentals.install.before_install"
-after_install = "rentals.install.after_install"
+# before_install = "rentals.install.before_install"
+# after_install = "rentals.install.after_install"
 
 # Uninstallation
 # ------------
